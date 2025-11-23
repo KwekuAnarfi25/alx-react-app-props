f1bb928a7c8a3149a4cb46a28fd2b65a5028dc90
Jedidiah Anarfi <janarfi25@gmail.com>
2025-11-23 13:55:06 +0000

    Delete alx-fe-reactjs directory

--- alx-fe-reactjs/alx-react-app-new/src/components/MainContent.jsx
+++
@@ -1,9 +0,0 @@
-function MainContent() {
-    return (
-        <main style={{ padding: '20px', backgroundColor: '#eef', minHeight: '300px' }}>
-            <p style={{ fontSize: '18px' }}>I love to visit New York, Paris, and Tokyo.</p>
-        </main>
-    );
-}
-
-export default MainContent;
